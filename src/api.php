c06ad0281f9339af2b83b9586cd551d26ffa717e
jarom <jb22@packetsmiter.com>
2018-07-26 14:58:47 -0700

    implemented get doc by id

--- src/api.php
+++ src/api.php
@@ -2,38 +2,52 @@
 require './config.php'; 
 define("FUTURE_TIME",'9999-12-31');
 $data;
+//main
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$input=json_decode($HTTP_RAW_POST_DATA);
 	$doc=$input->doc;
 	$table=preg_replace('/[^a-zA-Z0-9]/','',$input->table);
 	$data= array('table' => $table, 'messages' =>'','doc'=>$doc);
 	save_doc($doc,$table);
+} else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	$data['messages']='GET "' . $_GET['_id'] . '" from '. $_GET['table'];
+	$id=$_GET['_id'];
+	$table=$_GET['table'];
+	$data['doc']=get_doc($id,$table);
 }
 
 header('Content-Type: application/json');
 echo json_encode($data);
 
 function save_doc($doc,$table) {
-	echo 'save_doc ';
 	global $pdo, $data;
 	$old_doc = get_doc($doc->_id,$table);
 	$data['old_doc'] = $old_doc;
+	//echo json_encode($old_doc);
 	if (!$old_doc && !$doc->_rev) {
 		//insert
 		$sql="insert into $table (_id,_rev,doc,valid_from,valid_to) values(?,?,?,now(),?)";
 		$data['messages'] .= "new doc\nsql \"$sql\"";
 		$docstring=json_encode($doc);
-		$rev='1-' . hash($docstring);
-		$data['messages'] .= "\n _rev=$rev";
+		
+		$rev='1-' . sha1($docstring);
+		
 		$pdo->prepare($sql)->execute([
 			$doc->_id,
 			$rev,
 			json_encode($doc),
 			FUTURE_TIME
 		]);
+		$data['_rev']=$rev;
+		$data['_id']=$doc->_id;
 	} else if ($old_doc->_rev == $doc->_rev) {
 		//update
+		http_response_code(501);
 		echo 'update ';
+	} else {
+		http_response_code(409);
+		$data['http_response_code']=http_response_code();
+		$data['error'] = '_rev does not match';
 	}
 }
 function get_doc($id,$table) {
@@ -42,11 +56,11 @@ function get_doc($id,$table) {
 		$retval;
 		$sql="select * from $table where _id = ? and valid_from <= now() and valid_to > now()";
 		$stmt = $pdo->prepare($sql);
-		$stmt->execute([$doc->_id]);
+		$stmt->execute([$id]);
 		while ($row = $stmt->fetch()) {
-			$retval= $row['doc'];
-			$retval['_id']=$row['_id'];
-			$retval['_rev']=$row['_rev'];
+			$retval = json_decode($row['doc']);
+			$retval->_id=$row['_id'];
+			$retval->_rev=$row['_rev'];
 		}
 		return $retval;
 	} catch (PDOException $e) {
--- src/tests/spec/ApiSpec.js
+++ src/tests/spec/ApiSpec.js
@@ -5,26 +5,67 @@ describe("Api", function() {
 	  apiUrl=window.apiUrl;
   });
 
-  it("should be able to save a document", function() {
+  it("should be able to create a document", function() {
     var doc={
-		_id:'new doc '+new Date().getTime(),
-		a:'A',
-		b:'B',
-		bool:true
-	};
-	return fetch(apiUrl,{
-		method:'POST',
-		body:JSON.stringify({doc:doc,table:'table2'})
-	})
-	.then(res =>{ 
-		console.log(res);
-		return res.text();
-	})
-	.then(res => {
-		console.log('res',res);
-		expect(res._rev).toBeDefined();
-	});
-
+			_id:'new doc ' + new Date().getTime(),
+			a:'A',
+			b:'B',
+			bool:true
+		};
+		return fetch(apiUrl,{
+			method:'POST',
+			body:JSON.stringify({doc:doc,table:'table2'})
+		})
+		.then(res => res.json())
+		.then(res => {
+			//console.log('res',res);
+			expect(res._rev).toBeDefined();
+			return fetch(apiUrl+'?_id='+encodeURIComponent(doc._id)+'&table=table2');
+		})
+		.then(res=>res.json())
+		.then(res => {
+			//console.log('res',res);
+			var retrievedDoc=res.doc;
+			expect(retrievedDoc.a).toBe('A');
+		});
   });
+	it("should be able to update a document",function(){
+		var doc = {
+			_id:'update doc ' + new Date().getTime(),
+			a:'A',
+			b:'B',
+			bool:true
+		};
+		var rev='';
+		//save the doc
+		return fetch(apiUrl,{
+			method:'POST',
+			body:JSON.stringify({doc:doc,table:'table2'})
+		})
+		.then(res =>{ 
+			console.log(res);
+			return res.json();
+		})
+		.then(res => {
+			console.log('res',res);
+			expect(res._rev).toBeDefined();
+			rev=res._rev;
+			doc._rev=rev;
+			doc.c='C';
+			//save the doc again
+			return fetch(apiUrl,{
+				method:'POST',
+				body:JSON.stringify({doc:doc,table:'table2'})
+			});
+		})
+		.then(res => {
+			console.log(res);
+			return res.json();
+		})
+		.then(res => {
+			console.log(' updated res',res);
+			expect(res._rev).notToBe(rev);
 
+		});
+	})
 });
